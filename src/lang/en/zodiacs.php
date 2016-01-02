2ec80be2cd99a65e19590441566167e76aa4e4e4
Oliver Vogel <olivervogel@sprawl.fritz.box>
2016-01-02 21:40:41 +0100

    added english language file

---
+++ src/lang/en/zodiacs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'aries' => 'Aries',
+    'taurus' => 'Taurus',
+    'gemini' => 'Gemini',
+    'cancer' => 'Cancer',
+    'leo' => 'Leo',
+    'virgo' => 'Virgo',
+    'libra' => 'Libra',
+    'scorpio' => 'Scorpio',
+    'sagittarius' => 'Sagittarius',
+    'capricorn' => 'Capricorn',
+    'aquarius' => 'Aquarius',
+    'pisces' => 'Pisces'
+];

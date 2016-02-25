1b1dd0c3ee79d7245059d9efa4fc414041afe45b
Ryo Shibayama <j02521@gmail.com>
2016-02-25 20:23:27 +0900

    Add Japanese language

---
+++ src/lang/ja/zodiacs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'aries' => '牡羊座',
+    'taurus' => '牡牛座',
+    'gemini' => '双子座',
+    'cancer' => '蟹座',
+    'leo' => '獅子座',
+    'virgo' => '乙女座',
+    'libra' => '天秤座',
+    'scorpio' => '蠍座',
+    'sagittarius' => '射手座',
+    'capricorn' => '山羊座',
+    'aquarius' => '水瓶座',
+    'pisces' => '魚座'
+];

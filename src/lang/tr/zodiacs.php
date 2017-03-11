89f5f6849f831cc8ce84950673326f92c71c6ace
Ozgur KARAGOZ <turnthepage448@gmail.com>
2017-03-11 23:10:34 +0300

    Added Turkish zodiac names

---
+++ src/lang/tr/zodiacs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'aries' => 'Koç',
+    'taurus' => 'Boğa',
+    'gemini' => 'İkizler',
+    'cancer' => 'Yengeç',
+    'leo' => 'Aslan',
+    'virgo' => 'Başak',
+    'libra' => 'Terazi',
+    'scorpio' => 'Akrep',
+    'sagittarius' => 'Yay',
+    'capricorn' => 'Oğlak',
+    'aquarius' => 'Kova',
+    'pisces' => 'Balık'
+];

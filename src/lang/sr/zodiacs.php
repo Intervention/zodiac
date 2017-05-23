77f90bc9b13d2086e608fd9509cf60816bd38ef9
ergonomicus <ergonomicus@users.noreply.github.com>
2017-05-23 19:26:49 +0200

    Added serbian translations of zodiac signs.

---
+++ src/lang/sr/zodiacs.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'aries' => 'Ovan',
+    'taurus' => 'Bik',
+    'gemini' => 'Blizanci',
+    'cancer' => 'Rak',
+    'leo' => 'Lav',
+    'virgo' => 'Devica',
+    'libra' => 'Vaga',
+    'scorpio' => 'Škorpija',
+    'sagittarius' => 'Strelac',
+    'capricorn' => 'Jarac',
+    'aquarius' => 'Vodolija',
+    'pisces' => 'Ribe'
+];

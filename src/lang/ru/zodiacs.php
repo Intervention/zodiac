1bf17428dcced7d075c6213a2f72b91d9864cb1b
Zile <zile.lhs@gmail.com>
2017-07-14 03:47:01 +0200

    Add Russian translations.

---
+++ src/lang/ru/zodiacs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'aries' => 'Овен',
+    'taurus' => 'Телец',
+    'gemini' => 'Близнецы',
+    'cancer' => 'Рак',
+    'leo' => 'Лев',
+    'virgo' => 'Дева',
+    'libra' => 'Весы',
+    'scorpio' => 'Скорпион',
+    'sagittarius' => 'Стрелец',
+    'capricorn' => 'Козерог',
+    'aquarius' => 'Водолей',
+    'pisces' => 'Рыбы'
+];

cdc8a8b5f904350620087a1d829ce8a99f41d2c3
Oliver Vogel <oliver@olivervogel.com>
2017-07-20 16:09:47 +0200

    changed trait logic

--- src/Intervention/Zodiac/EloquentZodiacTrait.php
+++ src/Intervention/Zodiac/EloquentZodiacTrait.php
@@ -2,16 +2,17 @@
 
 namespace Intervention\Zodiac;
 
+use Zodiac;
+
 trait EloquentZodiacTrait
 {
     public function getZodiacAttribute()
     {
         $birthday = data_get($this->attributes, 'birthday');
-        $calculator = new Calculator(app('translator'));
 
         try {
             
-            return $calculator->make($birthday);    
+            return Zodiac::make($birthday);    
 
         } catch (Exceptions\NotReadableException $e) {
             return null;

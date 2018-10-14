a36bd38ab7da189b1c67e4cb9fe5109c471a3c54
A. Krumov <baceto90@gmail.com>
2018-10-14 11:12:15 +0300

    Bulgarian lang

---
+++ src/lang/bg/zodiacs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'aries'       => 'Овен',
+    'taurus'      => 'Телец',
+    'gemini'      => 'Близнаци',
+    'cancer'      => 'Рак',
+    'leo'         => 'Лъв',
+    'virgo'       => 'Дева',
+    'libra'       => 'Везни',
+    'scorpio'     => 'Скорпион',
+    'sagittarius' => 'Стрелец',
+    'capricorn'   => 'Козирог',
+    'aquarius'    => 'Водолей',
+    'pisces'      => 'Риби',
+];

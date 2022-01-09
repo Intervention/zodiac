e0d56b90518d504b21d5a593fbff70273b93dbd7
Oliver Vogel <oliver@olivervogel.com>
2022-01-09 16:27:22 +0100

    AbstractZodiacTest

--- tests/AbstractZodiacTest.php
+++ tests/AbstractZodiacTest.php
@@ -23,17 +23,9 @@ class AbstractZodiacTest extends TestCase
     public function testLocalized()
     {
         $zodiac = $this->getMockForAbstractClass(AbstractZodiac::class);
-        $zodiac->name = 'mock';
+        $zodiac->name = 'gemini';
 
-        $this->assertEquals('localized', $zodiac->localized());
-    }
-
-    public function testLocalizedWithoutTranslator()
-    {
-        $zodiac = $this->getMockForAbstractClass(AbstractZodiac::class);
-        $zodiac->name = 'mock';
-
-        $this->assertEquals('zodiacs.mock', $zodiac->localized());
+        $this->assertEquals('Gemini', $zodiac->localized());
     }
 
     public function testToString()

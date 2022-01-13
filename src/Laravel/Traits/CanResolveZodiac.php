108770413f4089ff483e580d5ac91921bb90c7a7
Oliver Vogel <oliver@olivervogel.com>
2022-01-13 16:02:16 +0100

    Fixed Eloquent Model Trait

--- src/Laravel/Traits/CanResolveZodiac.php
+++ src/Laravel/Traits/CanResolveZodiac.php
@@ -2,19 +2,21 @@
 
 namespace Intervention\Zodiac\Laravel\Traits;
 
+use Intervention\Zodiac\AbstractZodiac;
 use Intervention\Zodiac\Calculator as ZodiacCalculator;
 use Intervention\Zodiac\Exceptions\NotReadableException;
+use Intervention\Zodiac\Laravel\ZodiacFacade;
 
 trait CanResolveZodiac
 {
-    protected $zodiacAttribute = 'birthday';
+    protected $birthdayAttribute = 'birthday';
 
     public function getZodiacAttribute(): ?AbstractZodiac
     {
-        $birthday = data_get($this->attributes, $this->zodiacAttribute);
+        $birthday = data_get($this->attributes, $this->birthdayAttribute);
 
         try {
-            return ZodiacCalculator::make($birthday);
+            return ZodiacFacade::make($birthday);
         } catch (NotReadableException $e) {
             return null;
         }

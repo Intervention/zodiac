e7bf98cb06b2b4ceeb7bcfcfbd117673e90c8cfa
Oliver Vogel <oliver@olivervogel.com>
2025-11-17 15:39:43 +0100

    Fix bug

--- src/Laravel/Facades/Calculator.php
+++ src/Laravel/Facades/Calculator.php
@@ -4,17 +4,22 @@ declare(strict_types=1);
 
 namespace Intervention\Zodiac\Laravel\Facades;
 
+use Carbon\CarbonInterface;
+use DateTimeInterface;
 use Illuminate\Support\Facades\Facade;
+use Intervention\Zodiac\Astrology;
+use Intervention\Zodiac\Interfaces\SignInterface;
+use Stringable;
 
 /**
  * @method static \Intervention\Zodiac\Calculator western()
  * @method static \Intervention\Zodiac\Calculator chinese()
- * @method static \Intervention\Zodiac\Calculator withAstrology(Intervention\Zodiac\Astrology $astrology)
- * @method static \Intervention\Zodiac\Calculator fromString(string|Stringable $date, null|Intervention\Zodiac\Astrology = null)
- * @method static \Intervention\Zodiac\Calculator fromDate(DateTimeInterface $date, null|Intervention\Zodiac\Astrology = null)
- * @method static \Intervention\Zodiac\Calculator fromUnix(int|string $date, null|Intervention\Zodiac\Astrology = null)
- * @method static \Intervention\Zodiac\Calculator fromCarbon(Carbon\CarbonInterface $date, null|Intervention\Zodiac\Astrology = null)
- * @method static \Intervention\Zodiac\Calculator compare(Intervention\Zodiac\Interfaces\SignInterface $sign, Intervention\Zodiac\Interfaces\SignInterface $with)
+ * @method static \Intervention\Zodiac\Calculator withAstrology(Astrology $astrology)
+ * @method static \Intervention\Zodiac\Calculator fromString(string|Stringable $date, null|Astrology $astrology = null)
+ * @method static \Intervention\Zodiac\Calculator fromDate(DateTimeInterface $date, null|Astrology $astrology = null)
+ * @method static \Intervention\Zodiac\Calculator fromUnix(int|string $date, null|Astrology $astrology = null)
+ * @method static \Intervention\Zodiac\Calculator fromCarbon(CarbonInterface $date, null|Astrology $astrology = null)
+ * @method static \Intervention\Zodiac\Calculator compare(SignInterface $sign, SignInterface $with)
  */
 class Calculator extends Facade
 {

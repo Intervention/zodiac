4cc413da5d4f3f04b099e7fec9796b4bf2ddc558
Oliver Vogel <oliver@olivervogel.com>
2025-11-19 14:35:27 +0100

    Add return types

--- src/Laravel/Facades/Zodiac.php
+++ src/Laravel/Facades/Zodiac.php
@@ -12,14 +12,14 @@ use Intervention\Zodiac\Interfaces\SignInterface;
 use Stringable;
 
 /**
- * @method static \Intervention\Zodiac\Calculator western()
- * @method static \Intervention\Zodiac\Calculator chinese()
- * @method static \Intervention\Zodiac\Calculator withAstrology(Astrology $astrology)
- * @method static \Intervention\Zodiac\Calculator fromString(string|Stringable $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Calculator fromDate(DateTimeInterface $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Calculator fromUnix(int|string $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Calculator fromCarbon(CarbonInterface $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Calculator compare(SignInterface $sign, SignInterface $with)
+ * @method static \Intervention\Zodiac\Calculator western(): CalculatorInterface
+ * @method static \Intervention\Zodiac\Calculator chinese(): CalculatorInterface
+ * @method static \Intervention\Zodiac\Calculator withAstrology(Astrology $astrology): CalculatorInterface
+ * @method static \Intervention\Zodiac\Calculator fromString(string|Stringable $date, null|Astrology $astrology = null): SignInterface
+ * @method static \Intervention\Zodiac\Calculator fromDate(DateTimeInterface $date, null|Astrology $astrology = null): SignInterface
+ * @method static \Intervention\Zodiac\Calculator fromUnix(int|string $date, null|Astrology $astrology = null): SignInterface
+ * @method static \Intervention\Zodiac\Calculator fromCarbon(CarbonInterface $date, null|Astrology $astrology = null): SignInterface
+ * @method static \Intervention\Zodiac\Calculator compare(SignInterface $sign, SignInterface $with): float
  */
 class Zodiac extends Facade
 {

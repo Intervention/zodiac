a8c11de6bd1cb29cdea339d5e7d2f98daaae4000
Oliver Vogel <oliver@olivervogel.com>
2025-11-19 15:17:11 +0100

    Fix coding standards

--- src/Laravel/Facades/Zodiac.php
+++ src/Laravel/Facades/Zodiac.php
@@ -8,17 +8,18 @@ use Carbon\CarbonInterface;
 use DateTimeInterface;
 use Illuminate\Support\Facades\Facade;
 use Intervention\Zodiac\Astrology;
+use Intervention\Zodiac\Interfaces\CalculatorInterface;
 use Intervention\Zodiac\Interfaces\SignInterface;
 use Stringable;
 
 /**
- * @method static \Intervention\Zodiac\Interfaces\CalculatorInterface western()
- * @method static \Intervention\Zodiac\Interfaces\CalculatorInterface chinese()
- * @method static \Intervention\Zodiac\Interfaces\CalculatorInterface withAstrology(Astrology $astrology)
- * @method static \Intervention\Zodiac\Interfaces\SignInterface fromString(string|Stringable $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Interfaces\SignInterface fromDate(DateTimeInterface $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Interfaces\SignInterface fromUnix(int|string $date, null|Astrology $astrology = null)
- * @method static \Intervention\Zodiac\Interfaces\SignInterface fromCarbon(CarbonInterface $date, null|Astrology $astrology = null)
+ * @method static CalculatorInterface western()
+ * @method static CalculatorInterface chinese()
+ * @method static CalculatorInterface withAstrology(Astrology $astrology)
+ * @method static SignInterface fromString(string|Stringable $date, null|Astrology $astrology = null)
+ * @method static SignInterface fromDate(DateTimeInterface $date, null|Astrology $astrology = null)
+ * @method static SignInterface fromUnix(int|string $date, null|Astrology $astrology = null)
+ * @method static SignInterface fromCarbon(CarbonInterface $date, null|Astrology $astrology = null)
  * @method static float compare(SignInterface $sign, SignInterface $with)
  */
 class Zodiac extends Facade

4e7bca171d993589d4065b6a5e9c48da7e354564
Oliver Vogel <oliver@olivervogel.com>
2026-05-10 10:28:04 +0200

    Add translations

--- src/lang/zh-TW/zodiacs.php
+++ src/lang/zh-TW/zodiacs.php
@@ -15,4 +15,17 @@ return [
     \Intervention\Zodiac\Western\Signs\Capricorn::class => '魔羯座',
     \Intervention\Zodiac\Western\Signs\Aquarius::class => '水瓶座',
     \Intervention\Zodiac\Western\Signs\Pisces::class => '雙魚座',
+
+    \Intervention\Zodiac\Chinese\Signs\Rat::class => '鼠',
+    \Intervention\Zodiac\Chinese\Signs\Ox::class => '牛',
+    \Intervention\Zodiac\Chinese\Signs\Tiger::class => '虎',
+    \Intervention\Zodiac\Chinese\Signs\Rabbit::class => '兔',
+    \Intervention\Zodiac\Chinese\Signs\Dragon::class => '龍',
+    \Intervention\Zodiac\Chinese\Signs\Snake::class => '蛇',
+    \Intervention\Zodiac\Chinese\Signs\Horse::class => '馬',
+    \Intervention\Zodiac\Chinese\Signs\Goat::class => '羊',
+    \Intervention\Zodiac\Chinese\Signs\Monkey::class => '猴',
+    \Intervention\Zodiac\Chinese\Signs\Rooster::class => '雞',
+    \Intervention\Zodiac\Chinese\Signs\Dog::class => '狗',
+    \Intervention\Zodiac\Chinese\Signs\Pig::class => '豬',
 ];
